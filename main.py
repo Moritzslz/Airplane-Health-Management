56f02bfec669698500935b2e70a402eb3c405715
schultzm <schultzmoritz@gmail.com>
2024-02-09 17:17:31 +0100

    implemented util methods

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
+import pandas as pd
+from pyspark.sql import SparkSession
+import matplotlib.pyplot as plt
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# TODO dont forget to stop the SparkSession with spark.stop()
+
+pd_df = pd.read_csv("data/sample_sensor_data.csv")
+
+print(pd_df.head())
+
+spark = SparkSession.builder.appName("AirplaneHealthMonitoring").getOrCreate()
+
+df = spark.read.csv("data/sample_sensor_data.csv", header=True, inferSchema=True)
+
+df.show()
+
+df.printSchema()
+
+df.describe().show()
+
+temperature_df = df.filter(df["reading_type"] == "Temperature").toPandas()
+temperature_df.plot(x="timestamp", y="value", kind="line")
+plt.xlabel('Timestamp')
+plt.ylabel('Temperature')
+plt.title('Temperature Readings Over Time')
+plt.show()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def get_max(spark_df, reading_type, return_pandas_df):
+    max_value = spark_df.filter(spark_df["reading_type"] == reading_type).agg({"value": "max"}).collect()[0][0]
+    print(reading_type + "max value:", max_value)
+    if return_pandas_df:
+        return max_value.toPandas()
+    return max_value
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_min(spark_df, reading_type, return_pandas_df):
+    min_value = spark_df.filter(spark_df["reading_type"] == reading_type).agg({"value": "min"}).collect()[0][0]
+    print(reading_type + "min value:", min_value)
+    if return_pandas_df:
+        return min_value.toPandas()
+    return min_value
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def get_avg(spark_df, reading_type, return_pandas_df):
+    avg_value = spark_df.filter(spark_df["reading_type"] == reading_type).agg({"value": "avg"}).collect()[0][0]
+    print(reading_type + "avg value:", avg_value)
+    if return_pandas_df:
+        return avg_value.toPandas()
+    return avg_value
+
+
+def plot_data(title, x_label, y_label):
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.show()
+
+spark.stop()
