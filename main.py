eac0af46f918ffa96f0c8fa4b49d23495765287e
moritzslz <schultzmoritz@gmail.com>
2024-02-22 15:27:51 +0100

    Improved data visualisation and added traffic light feature

--- main.py
+++ main.py
@@ -1,127 +1,209 @@
+from datetime import datetime
+
 import pandas as pd
 from pyspark.sql import SparkSession
 import matplotlib.pyplot as plt
+from pyspark.sql.functions import col
 
-# TODO dont forget to stop the SparkSession with spark.stop()
-# Pandas:
-# Pandas DataFrame: Print first rows with .head() and last rows with .tail()
-# df.shape = number of rows and number of columns
-# shape is an attribute (array) of the df
-# df.info gives total number of rows, columns, datatypes and memory requirement
-# df.isnull().sum() to check for null values
-
-pd_df = pd.read_csv("data/flight_01_data.csv")
-
+# Loading the dataset
 spark = SparkSession.builder.appName("AirplaneHealthMonitoring").getOrCreate()
-
 spark_df = spark.read.csv("data/flight_01_data.csv", header=True, inferSchema=True)
 spark_df.printSchema()
 spark_df.describe().show()
 
-def create_plot_for_all_engines(number_of_engines):
+# Example thresholds
+thresholds_low = {
+    "voltage": "290",
+    "rotor_speed_rpm": "1000",
+    "thrust_newton": "50",
+    "temperature_celsius": "8"
+}
 
-    all_voltage_readings_by_engine = []
-    all_rpm_readings_by_engine = []
-    all_thrust_readings_by_engine = []
-    all_temperature_readings_by_engine = []
+thresholds_high = {
+    "voltage": "350",
+    "rotor_speed_rpm": "9000",
+    "thrust_newton": "200",
+    "temperature_celsius": "90"
+}
 
-    def create_plot_for_individual_engine(engine_id):
-            engine_df = spark_df.filter(spark_df["engine_id"] == engine_id).toPandas()
+"""
+This dictionary stores all engines and their high-level (traffic light) status
+Red: Readings have been out of bounds of the thresholds 
+Yellow: Readings have been within 10% of the thresholds
+Green: Readings ok.
+"""
+traffic_light = {}
 
-            timestamps = engine_df[engine_df["reading_type"] == "voltage"]["timestamp"]
-            voltage_readings = engine_df[engine_df["reading_type"] == "voltage"]["value"]
-            rpm_readings = engine_df[engine_df["reading_type"] == "rotor_speed_rpm"]["value"]
-            thrust_readings = engine_df[engine_df["reading_type"] == "thrust_newton"]["value"]
-            temperature_readings = engine_df[engine_df["reading_type"] == "temperature_celsius"]["value"]
 
-            all_voltage_readings_by_engine.append((timestamps, voltage_readings))
-            all_rpm_readings_by_engine.append((timestamps, rpm_readings))
-            all_thrust_readings_by_engine.append((timestamps, thrust_readings))
-            all_temperature_readings_by_engine.append((timestamps, temperature_readings))
+def analyse_engine_data(engine_id, begin_of_analysis, visualise):
+    """
+        This function analyses voltage, rpm, thrust and temperature readings
+        of an engine and alters if a certain threshold has been exceeded.
 
-            figure, axis = plt.subplots()
-            axis.plot(timestamps, voltage_readings, label="Voltage")
-            axis.plot(timestamps, rpm_readings, label="Rotor speed rpm")
-            axis.plot(timestamps, thrust_readings, label="Thrust in newton")
-            axis.plot(timestamps, temperature_readings, label="Temperature in celsius")
-            axis.set_title("Readings of " + engine_id)
-            axis.set_xlabel("Time")
-            axis.legend()
+        Parameters:
+        engine_id (String): Specify the engine you want to analise. Keep the format of the dataset in mind.
+        begin_of_analysis(String): Specify from which point on the data should be analysed; Format: 2024-02-09 08:00:00
+        visualise (Boolean):
 
-            plt.show()
+        Returns:
+        Console output giving high level information about the status of an engine
+        Optionally: Graph of engines data
+    """
 
-    for n in range(1, number_of_engines + 1):
-        engine_id = "engine_" + str(n)
-        create_plot_for_individual_engine(engine_id)
+    begin_datetime = datetime.strptime(begin_of_analysis, "%Y-%m-%d %H:%M:%S")
+    engine_df = spark_df.filter((col("engine_id") == engine_id) & (col("timestamp") >= begin_datetime))
 
-        # Plot for all engines' voltage readings
-    for index, (timestamps, voltage_readings) in enumerate(all_voltage_readings_by_engine):
-        plt.plot(timestamps, voltage_readings, label=index + 1)
-    plt.title("Voltage Readings of All Engines")
-    plt.xlabel("Time")
-    plt.ylabel("Voltage")
-    plt.legend()
+    # For testing in development
+    # engine_df.show()
+
+    timestamps = engine_df.select("timestamp").collect()
+    voltage_readings = engine_df.where(col("reading_type") == "voltage").select("value")
+    rpm_readings = engine_df.where(col("reading_type") == "rotor_speed_rpm").select("value")
+    thrust_readings = engine_df.where(col("reading_type") == "thrust_newton").select("value")
+    temperature_readings = engine_df.where(col("reading_type") == "temperature_celsius").select("value")
+
+    # Check if readings are within thresholds
+    check_threshold_and_alert(engine_id, voltage_readings, "voltage")
+    check_threshold_and_alert(engine_id, rpm_readings, "rotor_speed_rpm")
+    check_threshold_and_alert(engine_id, thrust_readings, "thrust_newton")
+    check_threshold_and_alert(engine_id, temperature_readings, "temperature_celsius")
+
+    if visualise:
+        visualise_engine_data(timestamps, voltage_readings, rpm_readings, thrust_readings, temperature_readings)
+
+
+def check_threshold_and_alert(engine_id, data, reading_type):
+    """
+        This function compares the data to the min and max thresholds and sets the traffic light for the engine.
+        An alert to the console is printed when the traffic light is set to yellow or red.
+
+        Parameters:
+        engine_id (String): Specify the engine you want to analise. Keep the format of the dataset in mind.
+        data(spark_df): readings of a specific type
+        reading_type (String): Exact type as specified in the thresholds dictionaries
+
+        Returns:
+        Console output giving high level information about the status of an engine
+
+    """
+    min_threshold = float(thresholds_low.get(reading_type))
+    max_threshold = float(thresholds_high.get(reading_type))
+
+    min_value = get_min(data)
+    max_value = get_max(data)
+
+    # Calculate 10% threshold values
+    min_threshold_110_percent = min_threshold * 1.1
+    max_threshold_90_percent = max_threshold * 0.9
+
+    # Traffic light: green
+    traffic_light[engine_id] = "green"
+
+    # Traffic light: yellow
+    if min_value <= min_threshold_110_percent:
+        traffic_light[engine_id] = "yellow"
+    if max_value >= max_threshold_90_percent:
+        traffic_light[engine_id] = "yellow"
+
+    # Traffic light: red
+    if min_value <= min_threshold:
+        alert(engine_id, reading_type, 0)
+        traffic_light[engine_id] = "red"
+    if max_value >= max_threshold:
+        alert(engine_id, reading_type, 1)
+        traffic_light[engine_id] = "red"
+
+
+def visualise_engine_data(timestamps, voltage_readings, rpm_readings, thrust_readings, temperature_readings):
+    time = [row['timestamp'] for row in timestamps]
+    time = list(dict.fromkeys(time))  # Removing all duplicates
+    voltage_readings = voltage_readings.toPandas()
+    rpm_readings = rpm_readings.toPandas()
+    thrust_readings = thrust_readings.toPandas()
+    temperature_readings = temperature_readings.toPandas()
+
+    voltage = voltage_readings['value'].tolist()
+    rpm = rpm_readings['value'].tolist()
+    thrust = thrust_readings['value'].tolist()
+    temperature = temperature_readings['value'].tolist()
+
+    plt.figure(figsize=(10, 6))
+
+    # Plot voltage readings
+    plt.subplot(2, 2, 1)
+    plt.plot(time, voltage, color='blue')
+    plt.title('Voltage Readings')
+    plt.xlabel('Timestamp')
+    plt.ylabel('Voltage')
+
+    # Plot RPM readings
+    plt.subplot(2, 2, 2)
+    plt.plot(time, rpm, color='green')
+    plt.title('Rotor Speed (RPM) Readings')
+    plt.xlabel('Timestamp')
+    plt.ylabel('Rotor Speed (RPM)')
+
+    # Plot thrust readings
+    plt.subplot(2, 2, 3)
+    plt.plot(time, thrust, color='red')
+    plt.title('Thrust (Newton) Readings')
+    plt.xlabel('Timestamp')
+    plt.ylabel('Thrust (Newton)')
+
+    # Plot temperature readings
+    plt.subplot(2, 2, 4)
+    plt.plot(time, temperature, color='orange')
+    plt.title('Temperature (Celsius) Readings')
+    plt.xlabel('Timestamp')
+    plt.ylabel('Temperature (Celsius)')
+
+    plt.tight_layout()
     plt.show()
 
-    # Plot for all engines' RPM readings
-    for index, (timestamps, rpm_readings) in enumerate(all_rpm_readings_by_engine):
-        plt.plot(timestamps, rpm_readings, label=index + 1)
-    plt.title("RPM Readings of All Engines")
-    plt.xlabel("Time")
-    plt.ylabel("Rotor Speed (RPM)")
-    plt.legend()
-    plt.show()
 
-    # Plot for all engines' thrust readings
-    for index, (timestamps, thrust_readings) in enumerate(all_thrust_readings_by_engine):
-        plt.plot(timestamps, thrust_readings, label=index + 1)
-    plt.title("Thrust Readings of All Engines")
-    plt.xlabel("Time")
-    plt.ylabel("Thrust (Newton)")
-    plt.legend()
-    plt.show()
+def alert(engine_id, reading_type, type_of_threshold):
+    """
+        This function analyses voltage, rpm, thrust and temperature readings
+        of an engine and alters if a certain threshold has been exceeded.
 
-    # Plot for all engines' temperature readings
-    for index, (timestamps, temperature_readings) in enumerate(all_temperature_readings_by_engine):
-        plt.plot(timestamps, temperature_readings, label=index + 1)
-    plt.title("Temperature Readings of All Engines")
-    plt.xlabel("Time")
-    plt.ylabel("Temperature (Celsius)")
-    plt.legend()
-    plt.show()
+        Parameters:
+        engine_id (String): Specify the engine you want to analise. Keep the format of the dataset in mind.
+        reading_type (String):
+        type (int): 0 == minimum threshold; 1 == maximum threshold
+
+        Returns:
+        Console output
+    """
+    print("############################################################")
+    print("Alert for " + engine_id.upper() + ":")
+    if type_of_threshold == 0:
+        print(reading_type.upper() + " readings LOWER than threshold.")
+    else:
+        print(reading_type.upper() + " readings HIGHER than threshold.")
+    print("############################################################")
 
 
-def get_max(spark_df, reading_type, return_pandas_df):
-    max_value = spark_df.filter(spark_df["reading_type"] == reading_type).agg({"value": "max"}).collect()[0][0]
-    print(reading_type + "max value:", max_value)
-    if return_pandas_df:
-        return max_value.toPandas()
-    return max_value
+def get_max(readings_df):
+    max_value = readings_df.agg({"value": "max"}).collect()[0][0]
+    return float(max_value)
 
 
-def get_min(spark_df, reading_type, return_pandas_df):
-    min_value = spark_df.filter(spark_df["reading_type"] == reading_type).agg({"value": "min"}).collect()[0][0]
-    print(reading_type + "min value:", min_value)
-    if return_pandas_df:
-        return min_value.toPandas()
-    return min_value
+def get_min(readings_df):
+    min_value = readings_df.agg({"value": "min"}).collect()[0][0]
+    return float(min_value)
 
 
-def get_avg(spark_df, reading_type, return_pandas_df):
-    avg_value = spark_df.filter(spark_df["reading_type"] == reading_type).agg({"value": "avg"}).collect()[0][0]
-    print(reading_type + "avg value:", avg_value)
-    if return_pandas_df:
-        return avg_value.toPandas()
-    return avg_value
+def get_avg(readings_df):
+    avg_value = readings_df.agg({"value": "avg"}).collect()[0][0]
+    return float(avg_value)
 
 
-def plot_data(title, x_label, y_label):
-    plt.title(title)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.show()
+if __name__ == '__main__':
+    begin_of_analysis = "2024-02-09 08:00:00"
+    for i in range(1, 10):
+        engine_id = "engine_" + str(i)
+        analyse_engine_data(engine_id, begin_of_analysis, True)
+    print(traffic_light)
 
-
-create_plot_for_all_engines(3)
 
 spark.stop()
